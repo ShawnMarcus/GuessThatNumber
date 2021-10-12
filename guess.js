c0b81fe534b4472b1b4a68a31e33c58221659ba4
Shawn Marcus <smarcus@post.com>
2021-10-12 11:14:47 -0400

    Created guess.js file. Created starting variables for project.

---
+++ guess.js
@@ -0,0 +1,21 @@
+// Guess That Number
+// Message to be used throughout the file/project
+const enterNumText = `Please enter a number greater than zero`;
+
+// For restarting the game
+let restartGame = true;
+
+// For storing the range of the number to be guessed/random number that will be created
+let rangeNum;
+
+// For storing the number to be guessed
+let randomNum;
+
+// For storing the number of attempts that the user has left
+let attempts;
+
+// For storing the user's guess
+let guess;
+
+// For storing user's response to play again or not play again
+let playAgain;
